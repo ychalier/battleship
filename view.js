adb514da8d0f1cad7879cc40c5402219209a0e3e
ychalier <yohan.chalier@telecom-paristech.fr>
2018-03-07 14:22:11 +0100

    added ship display

--- view.js
+++ view.js
@@ -1,3 +1,5 @@
+var CASE_SIZE = 30;
+
 class ViewCase {
   constructor(view, row, col) {
     this.view = view;
@@ -5,7 +7,30 @@ class ViewCase {
     this.col = col;
     this.el = document.createElement("div");
   }
+}
 
+class ViewShip {
+  constructor(view, model) {
+    this.view = view;
+    this.model = model;
+    this.el = document.createElement("div");
+    this.el.className = "ship";
+    var sub_div = document.createElement("div");
+    if (this.model.dir === 1) {
+      sub_div.style.width = (CASE_SIZE - 2) + "px";
+      sub_div.style.height = (this.model.size * CASE_SIZE - 2) + "px";
+      this.el.style.width = CASE_SIZE + "px";
+      this.el.style.height = (this.model.size * CASE_SIZE) + "px";
+    } else {
+      sub_div.style.width = (this.model.size * CASE_SIZE - 2) + "px";
+      sub_div.style.height = (CASE_SIZE - 2) + "px";
+      this.el.style.width = (this.model.size * CASE_SIZE) + "px";
+      this.el.style.height = CASE_SIZE + "px";
+    }
+    this.el.style.top = (this.model.row * CASE_SIZE) + "px";
+    this.el.style.left = (this.model.col * CASE_SIZE) + "px";
+    this.el.appendChild(sub_div);
+  }
 }
 
 class View {
@@ -22,11 +47,16 @@ class View {
     this.container.innerHTML = "";
     this.board_self = [];
     this.board_other = [];
-    for (var row=0; row<this.height; row++) {
-      for (var col=0; col<this.width; col++) {
+    for (var row = 0; row < this.height; row++) {
+      for (var col = 0; col < this.width; col++) {
         this.board_self.push(new ViewCase(this, row, col));
         this.board_other.push(new ViewCase(this, row, col));
       }
+    }
+    this.ships = [];
+    for (var s = 0; s < this.controller.model.ships_self.length; s++) {
+      var ship = this.controller.model.ships_self[s];
+      this.ships.push(new ViewShip(this, ship));
     }
     this.update();
   }
@@ -40,19 +70,27 @@ class View {
     }
   }
 
-  update_pregame() {
-    this.container.innerHTML = "";
+  create_blank_grid() {
     var board = document.createElement('div');
     board.className = "board";
     for (var row = 0; row < this.height; row++) {
       for (var col = 0; col < this.width; col ++) {
         var case_bg = document.createElement('div');
         case_bg.className = "case-bg";
-	board.appendChild(case_bg);
+        case_bg.appendChild(document.createElement('div'));
+	      board.appendChild(case_bg);
       }
+    }
+    return board;
+  }
+
+  update_pregame() {
+    this.container.innerHTML = "";
+    var board = this.create_blank_grid();
+    for (var s = 0; s < this.ships.length; s++) {
+      board.appendChild(this.ships[s].el);
     }
     this.container.appendChild(board);
   }
 
 }
-
